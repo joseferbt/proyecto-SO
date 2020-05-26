0c43f557f9c19af8bcbd0f27264e30a217aa56e6
Jose Fernando Botina <joseferbt@gmail.com>
2020-05-26 17:40:36 -0500

    date is running

---
+++ date.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int
+main(int argc, char *argv[])
+{
+  struct rtcdate r;
+
+  if (date(&r)) {
+    printf(2, "date failed\n");
+    exit();
+  }
+
+  printf(2, "%d:%d:%d de %d/%d/%d", r.hour,r.minute,r.second,r.day,r.month,r.year);
+
+  exit();
+}
